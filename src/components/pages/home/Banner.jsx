67fa237b2d8dd0ac794b03fd2fafffe46025e9ab
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-06-07 20:10:44 +0600

    Update banner part

--- src/components/pages/home/Banner.jsx
+++ src/components/pages/home/Banner.jsx
@@ -1,9 +1,16 @@
 const Banner = () => {
   return (
     <div className="bg-[#FFF8F5]">
-      <div className="max-w-screen-2xl px-8 mx-auto flex justify-between py-14">
-        <div className="">Left</div>
-        <div className="">Right</div>
+      <div className="max-w-screen-2xl px-8 mx-auto py-14">
+        <div className="flex justify-between">
+          <div className="">
+            <h1 className="text-4xl font-bold">
+              BEAUTY SALON <br />
+              FOR EVERY WOMEN
+            </h1>
+          </div>
+          <div className="">Right</div>
+        </div>
       </div>
     </div>
   );
